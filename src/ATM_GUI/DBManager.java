2224dbc2b99894ec24c13e214b900d23e76acaab
Yan <kudashovian2006@gmail.com>
2025-02-05 09:08:24 +0500

    Change DB Config

--- src/ATM_GUI/DBManager.java
+++ src/ATM_GUI/DBManager.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DBManager {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/atm_db?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/atm_db";
     private static final String USER = "root";
     private static final String PASSWORD = "qwerty";
 
